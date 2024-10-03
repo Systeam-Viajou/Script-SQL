9efb7bbf60b94f057f9567c99e842dce4d0899df
gabrieliolveira <146123637+gabrieliolveira@users.noreply.github.com>
2024-10-03 15:10:55 -0300

    Update triggers.sql

--- 2-Ano/script/triggers.sql
+++ 2-Ano/script/triggers.sql
@@ -5,19 +5,11 @@ ON plano
 FOR EACH ROW
 EXECUTE FUNCTION log_plano_func();
 
-
-CREATE TRIGGER trg_log_tour_virtual
+CREATE TRIGGER trg_log_evento
 AFTER INSERT OR UPDATE OR DELETE
-ON tour_virtual
+ON evento
 FOR EACH ROW
-EXECUTE FUNCTION log_tour_virtual_func();
-
-
-CREATE TRIGGER trg_log_eventos
-AFTER INSERT OR UPDATE OR DELETE
-ON eventos
-FOR EACH ROW
-EXECUTE FUNCTION log_eventos_func();
+EXECUTE FUNCTION log_evento_func();
 
 
 CREATE TRIGGER trg_log_excursao
@@ -26,13 +18,6 @@ ON excursao
 FOR EACH ROW
 EXECUTE FUNCTION log_excursao_func();
 
-
-CREATE TRIGGER trg_log_classificacao
-AFTER INSERT OR UPDATE OR DELETE
-ON classificacao
-FOR EACH ROW
-EXECUTE FUNCTION log_classificacao_func();
-
 CREATE TRIGGER trg_log_atracao
 AFTER INSERT OR UPDATE OR DELETE
 ON atracao
@@ -40,11 +25,16 @@ FOR EACH ROW
 EXECUTE FUNCTION log_atracao_func();
 
 
-
 CREATE TRIGGER trg_log_ponto_turistico
 AFTER INSERT OR UPDATE OR DELETE
 ON ponto_turistico
 FOR EACH ROW
 EXECUTE FUNCTION log_ponto_turistico_func();
+
+
+CREATE TRIGGER atualizar_media_apos_insercao
+AFTER INSERT OR UPDATE ON classificacao
+FOR EACH ROW
+EXECUTE FUNCTION atualizar_media_classificacao();
 
 
