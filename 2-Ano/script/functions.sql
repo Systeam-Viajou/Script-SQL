c0a65dc01c9b040c3b0ebff26701ee8d4961c1db
gabrieliolveira <146123637+gabrieliolveira@users.noreply.github.com>
2024-10-03 15:03:41 -0300

    Update functions.sql

--- 2-Ano/script/functions.sql
+++ 2-Ano/script/functions.sql
@@ -1,3 +1,4 @@
+-- Função de log para media_classificacao de
 CREATE OR REPLACE FUNCTION atualizar_media_classificacao()
 RETURNS TRIGGER AS $$
 DECLARE
@@ -14,66 +15,72 @@ BEGIN
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
-
+-- Função de log para 'plano'
 CREATE OR REPLACE FUNCTION log_plano_func()
 RETURNS TRIGGER AS $$
 BEGIN
     INSERT INTO log_plano (id_plano, operacao, data_operacao)
-    VALUES (NEW.ID, TG_OP, NOW());
-    RETURN NEW;
+    VALUES (COALESCE(NEW.ID, OLD.ID), TG_OP, NOW());
+    RETURN COALESCE(NEW, OLD);
 END;
 $$ LANGUAGE plpgsql;
 
+-- Função de log para 'tour virtual'
 CREATE OR REPLACE FUNCTION log_tour_virtual_func()
 RETURNS TRIGGER AS $$
 BEGIN
     INSERT INTO log_tour_virtual (id_tour_virtual, operacao, data_operacao)
-    VALUES (NEW.ID, TG_OP, NOW());
-    RETURN NEW;
+    VALUES (COALESCE(NEW.ID, OLD.ID), TG_OP, NOW());
+    RETURN COALESCE(NEW, OLD);
 END;
 $$ LANGUAGE plpgsql;
 
+-- Função de log para 'eventos'
 CREATE OR REPLACE FUNCTION log_eventos_func()
 RETURNS TRIGGER AS $$
 BEGIN
     INSERT INTO log_eventos (id_evento, operacao, data_operacao)
-    VALUES (NEW.ID, TG_OP, NOW());
-    RETURN NEW;
+    VALUES (COALESCE(NEW.ID, OLD.ID), TG_OP, NOW());
+    RETURN COALESCE(NEW, OLD);
 END;
 $$ LANGUAGE plpgsql;
 
+-- Função de log para 'excursão'
 CREATE OR REPLACE FUNCTION log_excursao_func()
 RETURNS TRIGGER AS $$
 BEGIN
     INSERT INTO log_excursao (id_excursao, operacao, data_operacao)
-    VALUES (NEW.ID, TG_OP, NOW());
-    RETURN NEW;
+    VALUES (COALESCE(NEW.ID, OLD.ID), TG_OP, NOW());
+    RETURN COALESCE(NEW, OLD);
 END;
 $$ LANGUAGE plpgsql;
 
+-- Função de log para 'classificação'
 CREATE OR REPLACE FUNCTION log_classificacao_func()
 RETURNS TRIGGER AS $$
 BEGIN
     INSERT INTO log_classificacao (id_classificacao, operacao, data_operacao)
-    VALUES (NEW.ID, TG_OP, NOW());
-    RETURN NEW;
+    VALUES (COALESCE(NEW.ID, OLD.ID), TG_OP, NOW());
+    RETURN COALESCE(NEW, OLD);
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION log_atracao()
+-- Função de log para 'atração'
+CREATE OR REPLACE FUNCTION log_atracao_func()
 RETURNS TRIGGER AS $$
 BEGIN
     INSERT INTO log_atracao (id_atracao, operacao, data_operacao)
-    VALUES (NEW.ID, TG_OP, NOW());
-    RETURN NEW;
+    VALUES (COALESCE(NEW.ID, OLD.ID), TG_OP, NOW());
+    RETURN COALESCE(NEW, OLD);
 END;
 $$ LANGUAGE plpgsql;
 
+-- Função de log para 'ponto turístico'
 CREATE OR REPLACE FUNCTION log_ponto_turistico_func()
 RETURNS TRIGGER AS $$
 BEGIN
-    INSERT INTO log_ponto_turistico (id_pónto_turistico, operacao, data_operacao)
-    VALUES (NEW.ID, TG_OP, NOW());
-    RETURN NEW;
+    INSERT INTO log_ponto_turistico (id_ponto_turistico, operacao, data_operacao)
+    VALUES (COALESCE(NEW.ID, OLD.ID), TG_OP, NOW());
+    RETURN COALESCE(NEW, OLD);
 END;
 $$ LANGUAGE plpgsql;
