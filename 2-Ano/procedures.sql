cf8394e9b177401eae658fd6fced100905618614
gabrieliolveira <146123637+gabrieliolveira@users.noreply.github.com>
2024-10-23 20:00:06 -0300

    Update procedures.sql

--- 2-Ano/procedures.sql
+++ 2-Ano/procedures.sql
@@ -17,13 +17,15 @@ BEGIN
 END;
 $$;
 
-CREATE OR REPLACE PROCEDURE avaliar_atracao(v_nota INT, v_id_usuario INT, v_id_atracao INT)
+CREATE OR REPLACE PROCEDURE avaliar_atracao(v_nota FLOAT, v_id_usuario INT, v_id_atracao INT)
 LANGUAGE plpgsql
 AS $$
 BEGIN
+    -- Validação da nota
     IF v_nota < 1 OR v_nota > 5 THEN
         RAISE EXCEPTION 'Nota inválida. Deve ser entre 1 e 5.';
     ELSE
+        -- Inserir a classificação
         INSERT INTO classificacao (nota, ID_usuario, ID_atracao)
         VALUES (v_nota, v_id_usuario, v_id_atracao);
     END IF;
