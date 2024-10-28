cc6e0b9fae0cb38166dabe575a42c2aa35bf666b
gabriel lenda <124402259+gbrlscosta@users.noreply.github.com>
2024-10-28 11:25:00 -0300

    Update procedures.sql

--- 2-Ano/procedures.sql
+++ 2-Ano/procedures.sql
@@ -12,3 +12,32 @@ BEGIN
     END IF;
 END;
 $$;
+
+CREATE OR REPLACE PROCEDURE atualizar_nome(v_uid_usuario VARCHAR, v_nickname VARCHAR)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    UPDATE usuario SET nickname = v_nickname
+    WHERE uid = v_uid_usuario;
+END;
+$$;
+
+CREATE OR REPLACE PROCEDURE cadastro_usuario(
+    v_uid_usuario VARCHAR,
+    v_nickname VARCHAR,
+    v_nome VARCHAR,
+    v_sobrenome VARCHAR,
+    v_cpf VARCHAR,
+    v_email VARCHAR,
+    v_data_nascimento DATE,
+    v_telefone VARCHAR,
+    v_genero CHAR,
+    v_senha VARCHAR
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO usuario (uid, nickname, nome, sobrenome, cpf, email, data_nascimento, telefone, genero, senha, id_role)
+    VALUES (v_uid_usuario, v_nickname, v_nome, v_sobrenome, v_cpf, v_email, v_data_nascimento, v_telefone, v_genero, v_senha, 2);
+END;
+$$;
